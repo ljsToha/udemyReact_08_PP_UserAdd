f354315afa99ec7fcc6ae74b8a5f06f31bd4afc7
ljsToha <ljstoha@naver.com>
2022-06-21 21:13:51 +0900

    UserAdd_02
    +enteredAge => 문자숫자를 숫자로 변경
    <button
          className={classes.button}
          type={props.type || 'button'}
          onClick={props.onClick}
        >
          {props.children}
        </button>

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../UI/Button';
 import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (
+      (enteredUsername.trim().length === 0 || enteredAge.trim().length) === 0
+    ) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input
+          id='username'
+          type='text'
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='age' type='number' />
-        <button type='submit'>Add User</button>
+        <input
+          id='age'
+          type='number'
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type='submit'>Add User</Button>
       </form>
     </Card>
   );
