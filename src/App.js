390331113beaa8ed087e9fcc2a9bb2f1df31e20e
ljsToha <ljstoha@naver.com>
2022-06-22 21:20:58 +0900

    UserAdd_03
    const [usersList, setUsersList] = useState([]);
    setUsersList((prevUserlist) => {
          return [...prevUserlist, { name: uName, age: uAge, id: Math.random().toString() }];
        });

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 const App = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserlist) => {
+      return [...prevUserlist, { name: uName, age: uAge, id: Math.random().toString() }];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 };
