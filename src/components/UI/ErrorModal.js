0b1eb176164224e5968ddb6314f80f3d31831f7c
ljsToha <ljstoha@naver.com>
2022-06-23 19:56:27 +0900

    UserAdd_03
    Modal Add

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Card from './Card';
+import Button from './Button';
+import classes from './ErrorModal.module.css';
+
+const ErrorModal = (props) => {
+  return (
+    <div>
+      <div className={classes.backdrop} onClick={props.onConfirm}></div>
+      {/* Modal 창 발생 시 뒤의 배경 눌러도 Modal 창 없어 짐 */}
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  );
+};
+
+export default ErrorModal;
